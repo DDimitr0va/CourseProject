46c44f5589a87a3231610bbeb34632601a42a143
DDimitr0va <77698495+DDimitr0va@users.noreply.github.com>
2021-04-26 20:07:46 +0300

    Ready Project

---
+++ src/main/java/com/yahoo/finance/validationErrorMessages/CreateAccountValidation.java
@@ -0,0 +1,46 @@
+package com.yahoo.finance.validationErrorMessages;
+
+
+import com.yahoo.finance.pages.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+public class CreateAccountValidation extends BasePage {
+    public CreateAccountValidation(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(id = "reg-error-yid")
+    private WebElement errorEmail;
+
+    @FindBy(id = "reg-error-password")
+    private WebElement errorPassword;
+
+    @FindBy(id = "reg-error-phone")
+    private WebElement errorPhone;
+
+    @FindBy(id = "reg-error-birthDate")
+    private WebElement errorBirthDate;
+
+    public String getEmailErrorMessage() {
+        executeOperationWithExplicitWait(10, ExpectedConditions.visibilityOf(errorEmail), 4);
+        return errorEmail.getText();
+    }
+
+    public String getPasswordErrorMessage() {
+        executeOperationWithExplicitWait(10, ExpectedConditions.visibilityOf(errorPassword), 4);
+        return errorPassword.getText();
+    }
+
+    public String getPhoneErrorMessage() {
+        executeOperationWithExplicitWait(10, ExpectedConditions.visibilityOf(errorPhone), 4);
+        return errorPhone.getText();
+    }
+
+    public String getBirthDateErrorMessage() {
+        executeOperationWithExplicitWait(10, ExpectedConditions.visibilityOf(errorBirthDate), 4);
+        return errorBirthDate.getText();
+    }
+}
